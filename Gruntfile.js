34dfe625c1cc866f96e2e6482d5a5d3998b65718
Steve Jamesson <steve@groovywebdesign.com>
2014-03-09 22:46:10 -0400

    exlucde .ds_store files on deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
     rsync: {
       options: {
         args: ['--verbose'],
-        exclude: ['.git'],
+        exclude: ['.git', '.DS_Store'],
         recursive: true
       },
       prod: {
