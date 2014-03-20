745c1c546c8db9486b415af5056f7f21898a7ccb
Steve Jamesson <steve@groovywebdesign.com>
2014-03-20 09:55:47 -0400

    update home page w/ welcome/intro

--- public/js/main.js
+++ public/js/main.js
@@ -3,11 +3,9 @@
   var App = {};
 
   App.formatDates = function() {
-    if ($('body').hasClass('page-article')) {
-      var pubdate = $('time[pubdate]'),
-          date = moment(new Date(pubdate.attr('datetime')));
-      pubdate.html(date.fromNow() + ' ');
-    }
+    var pubdate = $('time[pubdate]'),
+        date = moment(new Date(pubdate.attr('datetime')));
+    pubdate.html(date.fromNow() + ' ');
   };
 
   $(document).ready(function() {
